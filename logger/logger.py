1122e177762f942241036754b1da078bd9deffc8
guy elias <guy.tu0908@gmail.com>
2024-12-05 21:54:13 +0200

    made a log interface

---
+++ logger/logger.py
@@ -0,0 +1,13 @@
+class _Ilogger:
+    def __init__(self):
+        __setup()
+
+    def _setup(self):
+        pass
+
+    def log(self):
+        pass
+
+    def get_logged_data(self):
+        pass
+
