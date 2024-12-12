3d9dee02905245049d7a4b90b5e5a1a3514afa48
noam147 <noamar1234567@gmail.com>
2024-12-12 22:08:34 +0200

    put files of first sprint that didnt add

---
+++ SimpleWaf/Atest_routing.py
@@ -0,0 +1,55 @@
+import requests
+WAF_IP_ADDRESS = "127.0.0.1"
+PORT = 5000
+url = f"http://{WAF_IP_ADDRESS}:{PORT}"
+def t1est_1()->bool:
+    #does not work at this moment ->
+    #need to put https in url, and some headres are prablomatic
+    existing_web_host_name = "dns.google"
+    headers = {
+        "Host": existing_web_host_name
+    }
+    response = requests.get(url,headers=headers)
+    print(response)
+    assert response.status_code == 405
+def t1est_2()->bool:
+    not_existing_web_host_name = "my_not_existing_site.com"
+    headers = {
+        "Host": not_existing_web_host_name
+    }
+
+    response = requests.get(url,headers=headers)
+    print(response)
+    assert response.status_code == 402
+def t1est_3()->bool:
+    #does not work at this moment ->
+    #need to put https in url, and some headres are prablomatic
+    existing_web_host_name = "dns.google"
+    headers = {
+        "Host": existing_web_host_name
+    }
+    #malicus data:
+    data = {
+        "user":" 'or 1=1'"
+    }
+    response = requests.post(url,headers=headers,json=data)
+    print(response)
+    assert response.status_code == 400
+def t1est_4()->bool:
+    #does not work at this moment ->
+    #need to put https in url, and some headres are prablomatic
+    existing_web_host_name = "dns.google?user='or 1=1'"
+    headers = {
+        "Host": existing_web_host_name
+    }
+
+    response = requests.get(url,headers=headers)
+    print(response)
+    assert response.status_code == 400
+def activate_all_tests():
+    t1est_1()
+    t1est_2()
+    t1est_3()
+    t1est_4()
+if __name__ == '__main__':
+    activate_all_tests()
