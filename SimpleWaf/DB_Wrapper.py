08ed09e709d3a74ca5f62069d077d924bc45ffa6
noam147 <noamar1234567@gmail.com>
2024-12-14 22:52:05 +0200

    add testing and table of score db, not finished

--- SimpleWaf/DB_Wrapper.py
+++ SimpleWaf/DB_Wrapper.py
@@ -120,14 +120,21 @@ def create_tables()->None:
     exec_command(attackers_score_table)
     return
 #attackers score:
-def special_insert_or_update_attackers_score(attacker_ip_add:str)->None:
+def special_insert_or_update_attackers_score(attacker_ip_add:str,amount_to_add:float = 0.2)->None:
     """inserts new attacker or updates the score if the attacker already exists."""
     command = f"""
         INSERT INTO attackers_score (attacker_ip, current_score)
         VALUES (%s,{DEFAULT_STARTING_SCORE})
-        ON DUPLICATE KEY UPDATE current_score = current_score+0.2"""
+        ON DUPLICATE KEY UPDATE current_score = current_score + {amount_to_add}"""
     args = (attacker_ip_add,)
     exec_command(command,args)
+
+def reset_score_of_attacker(attacker_ip_add:str,initial_score:float=1.0):
+    command = """
+            UPDATE attackers_score SET current_score = %s where attacker_ip = %s
+            """
+    args = (initial_score,attacker_ip_add)
+    exec_command(command, args)
 def get_score_of_attacker(attacker_ip_add:str) -> float:
     """get current score of attacker
         NOTE: need to insert the attacker to the db and only after
@@ -137,11 +144,11 @@ def get_score_of_attacker(attacker_ip_add:str) -> float:
         """
     args = (attacker_ip_add,)
     result:list = exec_command(command, args)
-    if len(result) == 1:
+    if len(result) == 1 and result != ERROR_WITH_DB_EXEC_COMMAND_CODE:
         attacker_details = result[0]
         current_score = attacker_details[1]
-        return current_score
-    
+        return float(current_score)#we want type float, not dechimal
+
 #attackers:
 def insert_into_attackers(attacker_ip:str,date_to_free:str)->None:
 
@@ -151,6 +158,25 @@ def insert_into_attackers(attacker_ip:str,date_to_free:str)->None:
            """
     args = (attacker_ip,date_to_free)
     exec_command(command,args)
+def special_insert_or_update_attackers_table(attacker_ip:str,date_to_free:str)->None:
+    command = """
+               INSERT INTO attackers (attacker_ip, date_to_free) 
+               VALUES (%s, %s)
+               ON DUPLICATE KEY UPDATE date_to_free = %s
+               """
+    args = (attacker_ip, date_to_free, date_to_free)
+    exec_command(command, args)
+def get_date_to_free_of_attacker(attacker_ip:str) -> str:
+    command = """
+           SELECT * FROM attackers where attacker_ip = %s
+           """
+    args = (attacker_ip,)
+    result: list = exec_command(command, args)
+    if len(result) == 1 and result != ERROR_WITH_DB_EXEC_COMMAND_CODE:
+        attacker_details = result[0]
+        date_to_free = attacker_details[1]
+        return date_to_free
+    return ""
 def delete_attacker(attacker_ip:str)->None:
 
     command = """
--- SimpleWaf/DB_Wrapper_testing.py
+++ SimpleWaf/DB_Wrapper_testing.py
@@ -1,7 +1,33 @@
 import unittest
 import DB_Wrapper
 
+from datetime import datetime, timedelta
+def calc_n_days_from_now(n:int) -> str:
+
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=n)
+    formatted_date = future_date.strftime('%Y-%m-%d')
+    return formatted_date
+def calc_attacker_free_date(attacker_ip: str) -> str:
+    NUM_OF_DAYS = 30
+    ### update the score ###
+    DB_Wrapper.special_insert_or_update_attackers_score(attacker_ip)
+    ### get the current score ###
+    current_score = DB_Wrapper.get_score_of_attacker(attacker_ip)
+    DAYS_UNTIL_FREE:int = int(NUM_OF_DAYS * current_score)
+
+    return calc_n_days_from_now(DAYS_UNTIL_FREE)
+
+def when_find_attacker(attacker_ip: str):
+    free_date = calc_attacker_free_date(attacker_ip)
+    DB_Wrapper.special_insert_or_update_attackers_table(attacker_ip, free_date)
+
+
 class TestDB(unittest.TestCase):
+
+    def test_create_tables(self):
+        ### for creating everything needed before testing ###
+        DB_Wrapper.create_tables()
     def test_attackers_table(self):
         example_ip = "111.111.111.112"
 
@@ -31,7 +57,40 @@ class TestDB(unittest.TestCase):
         new_test_ip = DB_Wrapper.get_ip_address_by_host_name(example_host_name)
         if new_test_ip != new_ip:
             self.fail("ip didn't mach at updating")
+    def test_attackers_score_table(self):
+        example_ip = "222.222.222.112"
+        ### add attacker to db or increase its score ###
+        DB_Wrapper.special_insert_or_update_attackers_score(example_ip)
+        ### we want for the test to reset its score ###
+        DB_Wrapper.reset_score_of_attacker(example_ip)
+        current_score = DB_Wrapper.get_score_of_attacker(example_ip)
+        if current_score != 1:
+            self.fail("failed reset score")
+        ### test adding with defult amount ###
+        DB_Wrapper.special_insert_or_update_attackers_score(example_ip)
+        current_score = DB_Wrapper.get_score_of_attacker(example_ip)
+        if current_score != 1.2:
+            self.fail("failed updating score")
+        ### test with custom add amount ###
+        DB_Wrapper.special_insert_or_update_attackers_score(example_ip,amount_to_add=0.3)
+        current_score = DB_Wrapper.get_score_of_attacker(example_ip)
+        if current_score != 1.5:
+            self.fail("failed updating score")
+    def test_attackers_find(self):
+        example_ip = "123.123.123.123"
+        ### we want for the test to reset its score ###
+        DB_Wrapper.reset_score_of_attacker(example_ip)
 
+        when_find_attacker(example_ip)
+        current_score = DB_Wrapper.get_score_of_attacker(example_ip)
+        if current_score != 1.2:
+            self.fail("failed score")
+        free_date = DB_Wrapper.get_date_to_free_of_attacker(example_ip)
+        test_free_date_str = calc_n_days_from_now(int(30*1.2))
+        test_free_date = datetime.strptime(test_free_date_str, '%Y-%m-%d').date()
+        if free_date != test_free_date:
+            self.fail("free date is wrong")
 
 if __name__ == '__main__':
+
     unittest.main()
