19901c7628670c901c479ea5cd12e54b81d84215
guy elias <guy.tu0908@gmail.com>
2024-12-20 18:03:08 +0200

    added command line arguments to the tests and checked the functions

--- SimpleWaf/DB_Wrapper.py
+++ SimpleWaf/DB_Wrapper.py
@@ -14,6 +14,7 @@ db_config = {
 def drop_table(table_name:str):
     query = "DROP TABLE IF EXISTS " + table_name
     exec_command(query)
+
 def print_table_values(table_name:str):
     """func for debug"""
 
@@ -43,6 +44,7 @@ def print_table_values(table_name:str):
         if conn:
             conn.close()
         print("Connection closed.")
+
 def exec_command(command:str,args_for_command:tuple=()) -> list:
 
     """
@@ -53,12 +55,12 @@ def exec_command(command:str,args_for_command:tuple=()) -> list:
     output: if the command is an update/insert command - an empty list
     if the command is select command - list of the output
     """
-    db_config = {
+    """db_config = {
         "host": "localhost",
         "user": "user1",
         "password": "12345678",
         "database": "wafDataBase"
-    }
+    }"""
     result = []
     conn = None
     cursor = None
@@ -119,6 +121,7 @@ def create_tables()->None:
     exec_command(attackers_table)
     exec_command(attackers_score_table)
     return
+
 #attackers score:
 def special_insert_or_update_attackers_score(attacker_ip_add:str,amount_to_add:float = 0.2)->None:
     """inserts new attacker or updates the score if the attacker already exists."""
@@ -135,6 +138,7 @@ def reset_score_of_attacker(attacker_ip_add:str,initial_score:float=1.0):
             """
     args = (initial_score,attacker_ip_add)
     exec_command(command, args)
+
 def get_score_of_attacker(attacker_ip_add:str) -> float:
     """get current score of attacker
         NOTE: need to insert the attacker to the db and only after
@@ -158,6 +162,7 @@ def insert_into_attackers(attacker_ip:str,date_to_free:str)->None:
            """
     args = (attacker_ip,date_to_free)
     exec_command(command,args)
+
 def special_insert_or_update_attackers_table(attacker_ip:str,date_to_free:str)->None:
     command = """
                INSERT INTO attackers (attacker_ip, date_to_free) 
@@ -166,6 +171,7 @@ def special_insert_or_update_attackers_table(attacker_ip:str,date_to_free:str)->
                """
     args = (attacker_ip, date_to_free, date_to_free)
     exec_command(command, args)
+
 def get_date_to_free_of_attacker(attacker_ip:str) -> str:
     command = """
            SELECT * FROM attackers where attacker_ip = %s
@@ -177,6 +183,7 @@ def get_date_to_free_of_attacker(attacker_ip:str) -> str:
         date_to_free = attacker_details[1]
         return date_to_free
     return ""
+
 def delete_attacker(attacker_ip:str)->None:
 
     command = """
@@ -184,6 +191,7 @@ def delete_attacker(attacker_ip:str)->None:
                """
     args = (attacker_ip,)
     exec_command(command, args)
+
 def is_ip_blocked(user_ip:str)->bool:
 
     command = """
@@ -204,12 +212,14 @@ def insert_into_websites_ip(host_name:str,ip_add:str)->None:
                """
     args = (host_name,ip_add)
     exec_command(command,args)
+
 def update_website_ip(host_name:str,ip_add:str)->None:
     command = """
                   UPDATE websites_ip SET ip_address = %s where host_name = %s
                   """
     args = (ip_add, host_name)
     exec_command(command, args)
+
 def special_insert_or_update_website_ip(host_name:str,ip_add:str)->None:
     """inserts new website or updates the ip address if the website already exists."""
     command = """
@@ -242,6 +252,7 @@ def _hash_password(password:str):
     """func will encode the password before putting in db"""
     # maybe will be changes later into stronger hashing
     return hashlib.sha256(password)
+
 def verify_login_into_website_login(user_name:str,password:str) -> bool:
     """func will check if the user_name match the password in the db"""
     hashed_password = _hash_password(password)
@@ -253,6 +264,7 @@ def verify_login_into_website_login(user_name:str,password:str) -> bool:
     if len(result) == 1 and result != ERROR_WITH_DB_EXEC_COMMAND_CODE:
         return True
     return False
+
 def insert_into_website_login(host_name:str, user_name:str, password:str, email:str) -> bool:
     """func will perform sign up for new user
     :return true if insertion went without problems
@@ -267,9 +279,7 @@ def insert_into_website_login(host_name:str, user_name:str, password:str, email:
         return False
     return True
 
-def check_functunallty():
-
-
+def check_functionality():
     create_tables()
     print_table_values("attackers")
     #websites
