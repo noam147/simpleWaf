74fec3fd0c0b24c92f33d1ffd2e5cbda1c4df616
noam147 <noamar1234567@gmail.com>
2025-01-06 15:55:58 +0200

    add skeleton of file upload

---
+++ SimpleWaf/code/File_Upload_Preventer.py
@@ -0,0 +1,3 @@
+"""here we will not check for the file types, this is in the Attack scanner"""
+"""here we will eascape speacial characters in the file name"""
+"""here we will delete the ; char from the file to prevent reverse shell"""
