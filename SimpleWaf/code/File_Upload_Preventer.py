c3bd508d8628c27e511cf1cd1a3b2d8565a543c5
noam147 <noamar1234567@gmail.com>
2025-01-19 14:14:20 +0200

    add file names prevnter

--- SimpleWaf/code/File_Upload_Preventer.py
+++ SimpleWaf/code/File_Upload_Preventer.py
@@ -2,20 +2,29 @@
 """here we will eascape speacial characters in the file name"""
 """here we will delete the ; char from the file to prevent reverse shell"""
 from Attack_Preventer import Attack_Scanner
+from tornado.httputil import HTTPServerRequest
+import urllib.parse
 class File_Upload_Preventer(Attack_Scanner):
     @staticmethod
-    def replace_sql(text:str) -> str:
-        # result in chars that do not have the affect of ' in sql terms
-        text = text.replace("\"","&quot;")
-        text = text.replace("'","&#x27")
-        return text
+    def replace_file_name(name:str) -> str:
+        curr_name = name
+        curr_name = urllib.parse.unquote(curr_name)#get the actual string
+
+        ### delete slashes and semicolumn###
+        curr_name = curr_name.replace("/","")
+        curr_name = curr_name.replace("\\","")
+        curr_name = curr_name.replace(";","")
+        if len(curr_name) == 0:
+            return "UNNAMEDFILE"
+        return curr_name
     @staticmethod
-    def edit_request(request: Request) -> Request:
-        request.url = SQLI_Preventer.replace_sql(request.url)
-        new_headers = {}
-        for key,val in request.headers.items():
-            new_headers[key] = SQLI_Preventer.replace_sql(val)
-        request.headers = new_headers
-        if isinstance(request.data, str):#if data isn't str, we do not need to check it
-            request.data = SQLI_Preventer.replace_sql(request.data)
+    def edit_request(request: HTTPServerRequest) -> HTTPServerRequest:
+
+        """iterate over files and change their name by removing special chars"""
+        if 'files' in request.files:
+            # Iterate through the files sent in the request
+            for file_name, file_data_list in request.files.items():
+                for file_data in file_data_list:
+                    original_filename = file_data['filename']
+                    file_name['filename'] = File_Upload_Preventer.replace_file_name(original_filename)
         return request
--- SimpleWaf/code/SQLI_Prevnter.py
+++ SimpleWaf/code/SQLI_Prevnter.py
@@ -7,16 +7,15 @@ class SQLI_Preventer(Attack_Scanner):
     def replace_sql(text:str) -> str:
         # result in chars that do not have the affect of ' in sql terms
         text = text.replace("\"","&quot;")
-        text = text.replace("'","&#x27")
+        text = text.replace("'","&#x27;")
         return text
     @staticmethod
     def edit_request(request: HTTPServerRequest) -> HTTPServerRequest:
-        new_request = deepcopy(request)
-        new_request.uri = SQLI_Preventer.replace_sql(new_request.uri)
+        request.uri = SQLI_Preventer.replace_sql(request.uri)
         new_headers = {}
         for key,val in request.headers.items():
             new_headers[key] = SQLI_Preventer.replace_sql(val)
-        new_request.headers = new_headers
-        if new_request.body and isinstance(request.body, str):#if data isn't str, we do not need to check it
-            new_request.data = SQLI_Preventer.replace_sql(new_request.body)
-        return new_request
+        request.headers = new_headers
+        if request.body and isinstance(request.body, str):#if data isn't str, we do not need to check it
+            request.data = SQLI_Preventer.replace_sql(request.body)
+        return request
