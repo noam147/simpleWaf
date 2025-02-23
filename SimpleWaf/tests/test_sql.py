0197f0ac82abad4b048d427afc21be00fa19c3d3
guy elias <guy.tu0908@gmail.com>
2025-02-23 21:44:13 +0000

    Revert "Merge branch 'Fourth_Sprint' into 'Develop'"
    This reverts merge request !29

--- SimpleWaf/tests/test_sql.py
+++ SimpleWaf/tests/test_sql.py
@@ -1,14 +1,8 @@
 import unittest
 import urllib.parse
 import sys
-from typing import List
-
 sys.path.append('../code')
-import SQLI_Scanner_Utils
-
-RECOMMANDED_BANED_CHARECTERS: List[str] = ['\*', '\**', '--', '||', '&&', '**\\', '*\\', '/*', '/**', '*/', '**/']
-
-
+import SQLI_Scanner
 class TestSQLI(unittest.TestCase):
     def test_sqli(self):
         file_path = "../Intruder/detect/Generic_SQLI.txt"
@@ -18,8 +12,11 @@ class TestSQLI(unittest.TestCase):
         for line_sql in lines_sql_content:
             with self.subTest(line_sql=line_sql):
                 decoded_line = urllib.parse.unquote(line_sql)
-                if not SQLI_Scanner_Utils.find_sqli(decoded_line, banned_characters=RECOMMANDED_BANED_CHARECTERS):
-                        print("Did not find sql in this line: " + decoded_line)
+                result:bool = SQLI_Scanner.SqliScanner.scan(decoded_line)
+                if result == False:
+                    print("Did not find sql in this line: "+decoded_line)
+                #self.assertFalse(result)- program completly stops after that- shouldnot happend
+
 
 if __name__ == '__main__':
     unittest.main()
